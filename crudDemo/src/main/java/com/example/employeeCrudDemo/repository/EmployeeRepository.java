620324fd44e138f4f4d7f24d5e54b82ee890f4c7
Mai Eldash <maieldash0@gmail.com>
2024-05-01 12:16:51 +0300

    Add files via upload

---
+++ crudDemo/src/main/java/com/example/employeeCrudDemo/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.employeeCrudDemo.repository;
+
+import com.example.employeeCrudDemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
---
+++ crudDemo/src/main/java/com/example/employeeCrudDemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.employeeCrudDemo.service;
+
+import com.example.employeeCrudDemo.entity.Employee;
+import com.example.employeeCrudDemo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+        Employee employee = null;
+        if(result.isPresent()) {
+            employee = result.get();
+        }
+        else{
+            throw new RuntimeException("Employee not found");
+        }
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void delete(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
